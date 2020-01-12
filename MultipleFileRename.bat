dae38fba0f0d3a17acd322a51e883dbdbdecd27b
Meraj Ali <46266426+meraja3@users.noreply.github.com>
2020-01-12 15:12:41 +0300

    Update MultipleFileRename.bat

--- MultipleFileRename.bat
+++ MultipleFileRename.bat
@@ -1,12 +1,12 @@
 
-set message=YASREF-DL-CFIF-
-SET /A i= 200000
+set message=YASREF-DL-CFIF- //FileName 
+SET /A i= 200000 //for number increase
 :loop 
 
 IF %i%==200300 GOTO END 
 echo The value of i is %i% 
 
-ren *.pdf %message%%i%.pdf
+ren *.pdf %message%%i%.pdf //format with rename (eg. pdf format)
 
 SET /a i=%i%+1 
 GOTO :LOOP 
